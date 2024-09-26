88e3ac48117d28b8980a78595f8526eb78cb3dbb
alejandro <alejandro.ricog@alumnos.upm.es>
2024-09-26 13:37:51 +0200

    Creados getters y setters de player

--- src/main/java/upm/Player.java
+++ src/main/java/upm/Player.java
@@ -1,2 +1,31 @@
-package upm;public class Player {
+package upm;
+public class Player {
+    private String name;
+    private Double score;
+
+    public Player(String name,Double score){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
+    public String toString(){
+        return name+" "+score;
+    }
+
+
 }
