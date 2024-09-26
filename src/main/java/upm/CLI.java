1c6f9f92617b7bf57c2abbafd9bee63d2ed2fd89
EximpLettuce0 <eximplettuce@gmail.com>
2024-09-26 13:42:28 +0200

    Implementada clase CLI (scanner y print)

--- src/main/java/upm/CLI.java
+++ src/main/java/upm/CLI.java
@@ -1,2 +1,23 @@
-package upm;public class CLI {
+package upm;
+
+import java.util.Scanner;
+
+public class CLI {
+    /**
+     * Creates a new Scanner to read inputs.
+     *
+     * @return  a new Scanner object
+     */
+    public Scanner scanner() {
+        return new Scanner(System.in);
+    }
+
+    /**
+     * Prints the given string.
+     *
+     * @param s the string to be printed
+     */
+    public void print(String s) {
+        System.out.print(s);
+    }
 }
