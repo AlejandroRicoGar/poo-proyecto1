fa2be3dea1087a1f776aab0ac709204e168bafc8
Tigre2212 <t.oganesyan@alumnos.upm.es>
2024-09-27 14:23:17 +0200

    Corregido el constructor de la clase Match

--- src/main/java/upm/Match.java
+++ src/main/java/upm/Match.java
@@ -10,7 +10,7 @@ public class Match {
      * @param player1 Player object
      * @param player2 Player object
      */
-    public void match(Player player1, Player player2){
+    public Match (Player player1, Player player2){
         this.player1=player1;
         this.player2=player2;
     }
