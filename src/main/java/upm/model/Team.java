bf98c4d953df818421476f0b0ab74c94693cf879
alejandro <alejandro.ricog@alumnos.upm.es>
2024-11-05 15:14:59 +0100

    implementados metodos de Team

--- src/main/java/upm/model/Team.java
+++ src/main/java/upm/model/Team.java
@@ -1,4 +1,22 @@
 package upm.model;
 
+import upm.model.category.Category;
+
+import java.util.ArrayList;
+
 public class Team {
+    private String name;
+    private ArrayList<Player> members;
+
+    public Team(String name) {
+        this.name = name;
+        this.members = new ArrayList<>();
+    }
+
+    public void addMember(Player player) {
+        members.add(player);
+    }
+    public Double getCategory(Category category) {
+        return 0.0;
+    }
 }
