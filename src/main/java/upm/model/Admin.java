00f142f308f11744275180dbf46d966c0802108f
alejandro <alejandro.ricog@alumnos.upm.es>
2024-11-05 15:12:27 +0100

    implementados metodos de Admin

--- src/main/java/upm/model/Admin.java
+++ src/main/java/upm/model/Admin.java
@@ -6,4 +6,21 @@ public class Admin extends User{
     public Admin(String mail, String password, Users user) {
         super(password, mail, user);
     }
+
+    public void register(Player p){
+
+    }
+    public void register(Tournament t){
+
+    }
+    public void register(Team t){
+
+    }
+    public String matchmakingMan(Tournament t,String[] players){
+        return "";
+    }
+    public String matchmaking(Team t){
+        return "";
+    }
+
 }
