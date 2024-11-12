8b34d15fada0d4c90701dc93eb8cff20d7014db8
Palmier Hinojosa <victor.palmier@alumnos.upm.es>
2024-11-12 16:02:08 +0100

    implementado métodos básicos para usar PlayerController.java

--- src/main/java/upm/controller/PlayerController.java
+++ src/main/java/upm/controller/PlayerController.java
@@ -3,10 +3,27 @@ package upm.controller;
 import upm.model.Player;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class PlayerController {
     private ArrayList<Player> players;
 
-    public PlayerController() {}
+    public PlayerController() {
+        players = new ArrayList<>();
+    }
+
+    public void addPlayer (Player player){
+        players.add(player);
+    }
+
+    public Player search(String name){
+        Player player = null;
+        if(!players.isEmpty()){
+            for(Player p : players){
+                if(p.getName().equals(name)){
+                    player = p;
+                }
+            }
+        }
+        return player;
+    }
 }
