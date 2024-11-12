afe233a94ab096b0b456b823de92dddef7a4caf9
Palmier Hinojosa <victor.palmier@alumnos.upm.es>
2024-11-12 16:02:47 +0100

    implementado TeamAdd

--- src/main/java/upm/command/TeamAdd.java
+++ src/main/java/upm/command/TeamAdd.java
@@ -1,24 +1,40 @@
 package upm.command;
 
+import upm.controller.PlayerController;
 import upm.controller.TeamController;
 
-public class TeamAdd implements Command{
-    private TeamController admin;
-    public TeamAdd(TeamController admin) {
-        this.admin = admin;
+public class TeamAdd implements Command {
+
+    private TeamController teamController;
+    private PlayerController playerController;
+
+    public TeamAdd(TeamController teamController, PlayerController playerController) {
+        this.teamController = teamController;
+        this.playerController = playerController;
     }
     @Override
     public String apply(String[] stringsep) {
-        return "";
+        String output = "";
+        String[] params = stringsep[1].split(";");
+        if(params.length == 2) {
+            if (playerController.search(params[0]) != null) {
+                output = teamController.addPlayerToTeam(playerController.search(params[0]), teamController.search(params[1]));
+            } else {
+                output = "The player does not exist";
+            }
+        } else {
+            output = "Incorrect number of parameters";
+        }
+        return output;
     }
 
     @Override
     public String toString() {
-        return "";
+        return "> team-add PlayerName;TeamName";
     }
 
     @Override
     public String getCommand() {
-        return "";
+        return "team-add";
     }
 }
