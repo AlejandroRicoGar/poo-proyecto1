2383310afd3ee6739f2c7bc4fa08e4a0b906af2f
Palmier Hinojosa <victor.palmier@alumnos.upm.es>
2024-11-12 19:40:49 +0100

    added getTournaments to Member

--- src/main/java/upm/model/Member.java
+++ src/main/java/upm/model/Member.java
@@ -1,11 +1,12 @@
 package upm.model;
 
+import java.util.List;
+
 public interface Member {
     public String getName();
     public Double getCategory(Categories category);
     public void setCategory(Categories category,Double value);
     public void addTournament(Tournament tournament);
     public void deleteTournament(Tournament tournament);
-
-
+    public List<Tournament> getTournaments();
 }
