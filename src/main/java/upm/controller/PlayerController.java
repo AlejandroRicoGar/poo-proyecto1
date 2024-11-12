08790d8928e21588dac0d9533f1d9f29e9713c93
Palmier Hinojosa <victor.palmier@alumnos.upm.es>
2024-11-12 19:48:19 +0100

    added deletePlayer to PlayerController

--- src/main/java/upm/controller/PlayerController.java
+++ src/main/java/upm/controller/PlayerController.java
@@ -26,4 +26,19 @@ public class PlayerController {
         }
         return player;
     }
+
+    public String deletePlayer(String name){
+        String output = "";
+        Player player = search(name);
+        if(player != null) {
+            if (player.getTournaments().isEmpty()) {
+                players.remove(player);
+                output = "  Player " + name + " deleted successfully";
+            } else {
+                output = "  The player " + name + " is in a tournament, you must remove the player from the tournament before deleting it";
+            }
+        }
+        return output;
+    }
+
 }
