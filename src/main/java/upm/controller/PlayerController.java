531cfa18d24c394afdd0c7ee64a400e51e7901ec
Palmier Hinojosa <victor.palmier@alumnos.upm.es>
2024-11-13 12:41:52 +0100

    added getStatisticsCSV and getStatisticsJSON in PlayerController

--- src/main/java/upm/controller/PlayerController.java
+++ src/main/java/upm/controller/PlayerController.java
@@ -1,6 +1,7 @@
 package upm.controller;
 
 import upm.model.Admin;
+import upm.model.Categories;
 import upm.model.Player;
 
 import java.util.ArrayList;
@@ -56,6 +57,46 @@ public class PlayerController {
             Player player = new Player(name, surname, id, password, email, creator);
             players.add(player);
             output = "  Player " + name + " created successfully";
+        }
+        return output;
+    }
+
+    public String getStatisticsCSV(Player player) {
+        String output;
+        if (player != null) {
+            String format = "%-20s%-20s%-20s%-20s%-20s%n";
+            String header = String.format(format, "Scored Points", "Match Won", "Assists Points", "Past Tournaments", "Generated Money");
+    
+            double scoredPoints = player.getCategoryValue(Categories.SCORED_POINTS);
+            double matchWon = player.getCategoryValue(Categories.MATCH_WON);
+            double assistsPoints = player.getCategoryValue(Categories.ASISTS_POINTS);
+            double pastTournaments = player.getCategoryValue(Categories.PAST_TOURNAMENTS);
+            double generatedMoney = player.getCategoryValue(Categories.GENERATED_MONEY);
+    
+            String data = String.format(format, 
+                                        String.format("%.2f", scoredPoints), 
+                                        String.format("%.2f", matchWon), 
+                                        String.format("%.2f", assistsPoints), 
+                                        String.format("%.2f", pastTournaments), 
+                                        String.format("%.2f", generatedMoney));
+    
+            output = header + data;
+        } else {
+            output = "The player does not exist";
+        }
+        return output;
+    }
+    
+    public String getStatisticsJSON(Player player) {
+        String output;
+        if (player != null) {
+            output = "Scored Points: " + String.format("%.2f", player.getCategoryValue(Categories.SCORED_POINTS)) + ",\n" +
+                 "Match Won: " + String.format("%.2f", player.getCategoryValue(Categories.MATCH_WON)) + ",\n" +
+                 "Assists Points: " + String.format("%.2f", player.getCategoryValue(Categories.ASISTS_POINTS)) + ",\n" +
+                 "Past Tournaments: " + String.format("%.2f", player.getCategoryValue(Categories.PAST_TOURNAMENTS)) + ",\n" +
+                 "Generated Money: " + String.format("%.2f", player.getCategoryValue(Categories.GENERATED_MONEY));
+        } else {
+            output = "The player does not exist";
         }
         return output;
     }
