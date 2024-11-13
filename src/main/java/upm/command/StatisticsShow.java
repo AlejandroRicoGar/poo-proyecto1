66b81746646948f850b6e27a5f18d3c9ad918808
Palmier Hinojosa <victor.palmier@alumnos.upm.es>
2024-11-13 12:46:02 +0100

    documented javadoc of StatisticsShow

--- src/main/java/upm/command/StatisticsShow.java
+++ src/main/java/upm/command/StatisticsShow.java
@@ -1,17 +1,34 @@
+/**
+ * Command to show the statistics of a player
+ */
 package upm.command;
 
 import upm.controller.PlayerController;
 import upm.controller.PublicController;
 import upm.model.Player;
 
+/**
+ * Class to show the statistics of a player
+ */
 public class StatisticsShow implements Command{
     private PlayerController playerController;
     private PublicController publicController;
 
+    /**
+     * Constructor of the class
+     * @param playerController the player controller
+     * @param publicController the public controller
+     */
     public StatisticsShow(PlayerController playerController, PublicController publicController) {
         this.playerController = playerController;
         this.publicController = publicController;
     }
+
+    /**
+     * Applies the command
+     * @param stringsep the parameters of the command "statistics-show -csv/-json"
+     * @return the output of the command
+     */
     @Override
     public String apply(String[] stringsep) {
         String output = "";
@@ -31,13 +48,22 @@ public class StatisticsShow implements Command{
 
     }
 
+    /**
+     * Returns a string with the command description
+     * @return the command description
+     */
     @Override
     public String toString() {
         return "> statistics-show -csv/-json";
     }
 
+    /**
+     * Returns the command name
+     * @return the command name
+     */
     @Override
     public String getCommand() {
         return "statistics-show";
     }
 }
+
