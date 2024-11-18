83b56a107c039609127656c287d3233a989db990
Tigre2212 <t.oganesyan@alumnos.upm.es>
2024-11-18 18:29:38 +0100

    Terminada y documentada clase TeamCreate

--- src/main/java/upm/command/TeamCreate.java
+++ src/main/java/upm/command/TeamCreate.java
@@ -3,14 +3,32 @@ package upm.command;
 import upm.controller.PublicController;
 import upm.controller.TeamController;
 import upm.model.Team;
-
+/**
+ * Class to create a team
+ */
 public class TeamCreate implements Command{
+    /**
+     * Team controller to create team
+     */
     private TeamController teamController;
+    /**
+     * Public controller to get the admin
+     */
     private PublicController publicController;
+    /**
+     * Constructor of the class
+     * @param tcontroller Team controller to create the team
+     * @param pcontroller Public controller to get the admin
+     */
     public TeamCreate(TeamController tcontroller,PublicController pcontroller) {
         this.teamController = tcontroller;
         this.publicController = pcontroller;
     }
+    /**
+     * Creates a team
+     * @param stringsep An array with the parameters [team-create, name]
+     * @return The output of the command
+     */
     @Override
     public String apply(String[] stringsep) {
             String output = "";
@@ -25,12 +43,16 @@ public class TeamCreate implements Command{
             }
             return output;
     }
-
+    /**
+     * @return String with command description
+     */
     @Override
     public String toString() {
         return "";
     }
-
+    /**
+     * @return the name of the command
+     */
     @Override
     public String getCommand() {
         return "team-create";
