bf9dd4bf5fe85abf839f9a58f95c900814170c11
Tigre2212 <t.oganesyan@alumnos.upm.es>
2024-11-19 11:08:45 +0100

    Terminada sin documentar clase TeamRemove

--- src/main/java/upm/command/TeamRemove.java
+++ src/main/java/upm/command/TeamRemove.java
@@ -1,24 +1,40 @@
 package upm.command;
 
 import upm.controller.TeamController;
+import upm.model.Team;
 
 public class TeamRemove implements Command{
-    private TeamController admin;
-    public TeamRemove(TeamController admin) {
-        this.admin = admin;
+    private TeamController teamController;
+    public TeamRemove(TeamController tcontroller) {
+        this.teamController = tcontroller;
     }
     @Override
     public String apply(String[] stringsep) {
-        return "";
+        String output = "";
+        if(stringsep.length == 2) {
+            String[] params = stringsep[1].split(";");
+            Team team=teamController.searchTeam(params[0]);
+            if(team!=null)
+                if(team.getMember(params[1])!=null){
+                    output= teamController.deletePlayerFromTeam(team.getMember(params[1]),team);
+                }else
+                    output="Team does not have the player";
+            else
+                output="Team doesnt exist";
+        }
+        else{
+            output = "Incorrect number of parameters, expected team name and playerId";
+        }
+        return output;
     }
 
     @Override
     public String toString() {
-        return "";
+        return "team-remove teamname;playerId";
     }
 
     @Override
     public String getCommand() {
-        return "";
+        return "team-remove";
     }
 }
