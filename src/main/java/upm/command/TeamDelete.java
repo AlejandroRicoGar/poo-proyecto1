db4875e9bac8e16e909cd9add7f0918034cc1526
Tigre2212 <t.oganesyan@alumnos.upm.es>
2024-11-19 09:27:52 +0100

    Metodo apply de la clase TeamDelete

--- src/main/java/upm/command/TeamDelete.java
+++ src/main/java/upm/command/TeamDelete.java
@@ -1,16 +1,31 @@
 package upm.command;
 
 import upm.controller.TeamController;
+import upm.model.Team;
 
 public class TeamDelete implements Command{
-    private TeamController admin;
+    private TeamController teamController;
 
-    public TeamDelete(TeamController admin) {
-        this.admin = admin;
+    public TeamDelete(TeamController tcontroller) {
+        this.teamController = tcontroller;
     }
     @Override
     public String apply(String[] stringsep) {
-        return "";
+        String output = "";
+        if(stringsep.length == 2) {
+            Team team=teamController.searchTeam(stringsep[1]);
+            if(team!=null)
+                if(team.getTournaments().isEmpty())
+                    output=teamController.deleteTeam(team);
+                else
+                    output="Team cannot be deleted if it is in a tournament";
+            else
+                output="Team doesnt exist";
+        }
+        else{
+            output = "Incorrect number of parameters, expected only team name";
+        }
+        return output;
     }
 
     @Override
