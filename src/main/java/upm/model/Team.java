a32deae7cb39faccf6702aaba597457e2000c247
Tigre2212 <t.oganesyan@alumnos.upm.es>
2024-11-19 10:23:54 +0100

    Creado metodo isMember(playerID) en clase Team

--- src/main/java/upm/model/Team.java
+++ src/main/java/upm/model/Team.java
@@ -5,14 +5,14 @@ import java.util.List;
 
 public class Team implements Member{
     private String name;
-    private ArrayList<Player> members;
+    private ArrayList<Player> teammates;
 
     private ArrayList<Tournament> tournaments;
     private Admin creator;
 
     public Team(String name,Admin creator) {
         this.name = name;
-        this.members = new ArrayList<>();
+        this.teammates = new ArrayList<>();
         this.tournaments = new ArrayList<>();
         this.creator=creator;
     }
@@ -22,37 +22,47 @@ public class Team implements Member{
     }
 
     public Boolean isMember(Player player) {
-        return members.contains(player);
+        return teammates.contains(player);
+    }
+    public Boolean isMember(String playerID) {
+        boolean resul = false;
+        for(Player player: teammates){
+            if(player.getId().equals(playerID)){
+                resul=true;
+                break;
+            }
+        }
+        return resul;
     }
 
     public void addMember(Player player) {
-        members.add(player);
+        teammates.add(player);
     }
     @Override
     public Double getCategoryValue(Categories categoria) {
         Double total = 0.0;
-        for(Player member : members) {
+        for(Player member : teammates) {
             total *= member.getCategoryValue(categoria);
         }
-        return Math.pow(total,((double) 1 /members.size()));
+        return Math.pow(total,((double) 1 / teammates.size()));
     }
 
     @Override
     public void setCategoryValue(Categories category, Double value) {
-        for (Player member : members) {
+        for (Player member : teammates) {
             member.setCategoryValue(category, value);
         }
     }
     @Override
     public void addTournament(Tournament tournament) {
-        for(Player member : members) {
+        for(Player member : teammates) {
             member.addTournament(tournament);
         }
     }
 
     @Override
     public void deleteTournament(Tournament tournament) {
-        for(Player member : members) {
+        for(Player member : teammates) {
             member.deleteTournament(tournament);
         }
     }
