fddcb357b21b2fc2c42647b0b182d7060069f448
Palmier Hinojosa <victor.palmier@alumnos.upm.es>
2024-11-20 14:14:51 +0100

    modified PlayerCreate to comply with singleton implementation of PlayerController

--- src/main/java/upm/command/PlayerCreate.java
+++ src/main/java/upm/command/PlayerCreate.java
@@ -11,24 +11,6 @@ import upm.controller.PublicController;
  */
 public class PlayerCreate implements Command {
     /**
-     * Player controller to create the player
-     */
-    private PlayerController playerController;
-    /**
-     * Public controller to get the admin that is logged
-     */
-
-
-    /**
-     * Constructor of the class
-     * @param playerController Player controller to create the player
-     */
-    public PlayerCreate(PlayerController playerController) {
-        this.playerController = playerController;
-
-    }
-
-    /**
      * Creates a player
      * @param stringsep An array with the parameters "player-create name;surname;id;password;email"
      * @return The output of the command
@@ -38,9 +20,9 @@ public class PlayerCreate implements Command {
         String output = "";
         String[] params = stringsep[1].split(";");
         if(params.length == 5) {
-            output = playerController.createPlayer(params[0], params[1], params[2], params[3], params[4], PublicController.getInstance().getAdmin(PublicController.getInstance().getLogged()));
-            if (playerController.search(params[2]) != null) {
-                PublicController.getInstance().signUpUser(playerController.search(params[2]));
+            output = PlayerController.getInstance().createPlayer(params[0], params[1], params[2], params[3], params[4], PublicController.getInstance().getAdmin(PublicController.getInstance().getLogged()));
+            if (PlayerController.getInstance().search(params[2]) != null) {
+                PublicController.getInstance().signUpUser(PlayerController.getInstance().search(params[2]));
             }
         } else {
             output = "Incorrect number of parameters";
