82f6187b48cd8b6a4f6a7218170344738987d502
Palmier Hinojosa <victor.palmier@alumnos.upm.es>
2024-11-24 18:18:21 +0100

    removed unnecessary methods from admin

--- src/main/java/upm/model/Admin.java
+++ src/main/java/upm/model/Admin.java
@@ -4,21 +4,4 @@ public class Admin extends User{
     public Admin(String mail, String password) {
         super(password, mail, Users.ADMIN);
     }
-
-    public void register(Player p){
-
-    }
-    public void register(Tournament t){
-
-    }
-    public void register(Team t){
-
-    }
-    public String matchmakingMan(Tournament t,String[] players){
-        return "";
-    }
-    public String matchmaking(Team t){
-        return "";
-    }
-
 }
