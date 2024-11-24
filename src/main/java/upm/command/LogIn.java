2e643e7908928944e088101510a88e60546702a3
EximpLettuce0 <eximplettuce@gmail.com>
2024-11-24 17:38:58 +0100

    cleaned up the code from LogIn

--- src/main/java/upm/command/LogIn.java
+++ src/main/java/upm/command/LogIn.java
@@ -6,27 +6,31 @@ public class LogIn implements Command{
 
 
     /**
-     * Metodo que comprueba que los parametros que se pasan es correcto y que el usuario no estan logueado ya
-     * @param params Division por espacios el param[0] es el comando en si y el param[1] son los argumentos
-     * @return
+     * Logs a user in
+     * @param params  An array of strings containing the user's email and password.
+     * @return A string indicating the result of the login attempt.
      */
     @Override
     public String apply(String[] params) {
         String output = "";
-        if(!PublicController.getInstance().isAdmin() && !PublicController.getInstance().isPlayer()) {
+
+        if (!(PublicController.getInstance().isAdmin() || PublicController.getInstance().isPlayer())) {
+
             if (params.length == 2) {
-                String[] args = params[1].split(";");
-                if (args.length == 2) {
-                    output = PublicController.getInstance().login(args);
-                }else{
+                String[] arguments = params[1].split(";");
+
+                if (arguments.length == 2) {
+                    output = PublicController.getInstance().login(arguments);
+                } else {
                     output = "Incorrect number of arguments.";
                 }
             } else {
                 output = "Incorrect number of arguments";
             }
-        }else{
+        } else {
             output = "You are already logged in, if you wish to use another account logout and login again";
         }
+
         return output;
     }
 
@@ -40,3 +44,4 @@ public class LogIn implements Command{
         return "login";
     }
 }
+
